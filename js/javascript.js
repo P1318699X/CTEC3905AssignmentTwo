9ea86fc34cc5ba149841c3c8cb44fa5d90393752
cbenjamin1993 <P1318699X@my365.dmu.ac.uk>
2017-03-03 14:32:09 +0000

    Fixed carousel so that text blocks change with the images.

--- js/javascript.js
+++ js/javascript.js
@@ -14,11 +14,13 @@ document.onload = (function(){
     forwardButtonVar.addEventListener("click", forwardBtn_Clicked);
 
     // For the carousel
-    var /* Index counter for the carousel */      index = 0, 
-        /* All the image DIVs in the container */ images = $('#imageContainer div'), 
-        /* The number of DIVs in the container */ imageCount = images.length;
+    var /* Index counter for the carousel */                index = 0, 
+        /* All the image DIVs in the container */           images = $('#imageContainer div'), 
+        /* The number of DIVs in the image container */     imageCount = images.length,
+        /* All the text DIVs in the container */            blocks = $('#textContainer div'),
+        /* The number of DIVs in the text container*/       blockCount = text.length;
 
-    function moveImages()
+    function moveItems()
     {
         // If the index is less than zero, wrap around to the highest item in the list.
         if      (index < 0)              index = imageCount - 1;
@@ -26,10 +28,13 @@ document.onload = (function(){
         else if (index > imageCount - 1) index = 0;
         // Finds out what the image is being shown at the current index.
         var image = $('#imageContainer div').eq(index);
+        var block = $('#textContainer div').eq(index);
         // Hides the other images in the container.
         images.hide();
+        blocks.hide();
         // Sets the image at the index in question to show.
         image.css('display','inline-block');
+        block.css('display','inline-block');
     }
 
     // Back Button
@@ -37,7 +42,7 @@ document.onload = (function(){
     {
         //Index Decreased by 1
         index -= 1;
-        moveImages();
+        moveItems();
     }
 
     // Forward Button
@@ -45,6 +50,6 @@ document.onload = (function(){
     {
         //Index Increased by 1
         index += 1;
-        moveImages();
+        moveItems();
     }
 }());
